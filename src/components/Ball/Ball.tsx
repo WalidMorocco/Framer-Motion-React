1433a7691425616e3eafc1fbb955f39d81c91d5b
Walid Amar <walidamarpro@gmail.com>
2022-09-26 03:41:27 -0400

    motion up succeded

--- src/components/Ball/Ball.tsx
+++ src/components/Ball/Ball.tsx
@@ -3,29 +3,13 @@ import { useEffect, useRef, useState } from "react";
 
 
 export const Ball = () => {
-  const scrollRef = useRef(null)
-  const { scrollY } = useScroll({
-    container: scrollRef
-  });
-  const [hidden, setHidden] = useState(false);
-  
-  // function update() {
-  //   if (scrollY?.current < scrollY?.prev) {
-  //     setHidden(false);
-  //   } else if (scrollY?.current > 100 && scrollY?.current > scrollY?.prev) {
-  //     setHidden(true);
-  //   }
-  // }
+ 
 
-  useEffect(() => {
-    console.log(scrollY);
-  },[scrollY]);
+  
 
   return ( 
-    <div className="grid place-items-center h-screen ">
-      <motion.div  ref={scrollRef} className=" grid place-items-center h-80 w-80 bg-red-400 rounded-[50%]"
-        // variants={variants}
-        animate={hidden ? "hidden" : "visible"}
+    <div className="grid place-items-center h-full">
+      <motion.div   className=" grid place-items-center h-80 w-80 bg-red-400 rounded-[50%]"
         transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
       >
         <motion.div className="h-48 w-48 bg-red-600 rounded-[50%] "
--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,15 +1,69 @@
-import { motion } from "framer-motion"
+import { motion, useScroll, Variants } from "framer-motion"
+import { useEffect, useState } from "react";
 import { Ball } from "./Ball/Ball";
 import { Grid } from "./Grid/Grid";
 import { Jauge } from "./Jauge/Jauge";
+import Nav from "./Nav/Nav";
+
+const ball: Variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 }
+  },
+  closed: { opacity: 0, y: -200, transition: { duration: 0.5 } }
+};
+
+const rest: Variants = {
+  open: { opacity: 1, y: -600, transition: { duration: 0.5 } },
+  closed: { opacity: 0, y: 0, transition: { duration: 0.5 } }
+};
 
 export const MyComponent = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  
+
+  function buttonClick() {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+    console.log()
+  }
+
+  // useEffect(() => {
+  //   return scrollYProgress.onChange((latest) => {
+  //     if (latest > 0) {
+  //       setIsOpen(false);
+  //       console.log(x.hasAnimated);
+  //     } if (latest = 0) {
+  //       setIsOpen(true)
+  //     }
+  //   })
+  // }, [])
+  
   return ( 
-    <div className="space-y-96">
-      <Ball />
-      <Grid />
-      <Jauge />
-    </div>
+    <motion.div className="">
+      <motion.button
+        whileTap={{ scale: 0.97 }}
+        onClick={() => buttonClick()}
+      >
+        Click
+      </motion.button>
+
+      <motion.div
+      variants={ball}
+      animate={isOpen ? "open" : "closed"}
+      className="h-screen">
+        <Ball />
+      </motion.div>
+
+      <motion.div variants={rest}
+      animate={isOpen ? "closed" : "open"}
+      className="h-screen">
+        <Grid />
+        <Jauge />
+      {/* <Nav/> */}
+      </motion.div>
+    </motion.div>
   )
   
 }
